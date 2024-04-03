c84e358e6894c06758b785246f3189d11aea9879
Joseph Spisak <spisakjo@gmail.com>
2024-04-03 11:45:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+# This software may be used and distributed according to the terms of the Llama 3 Community License Agreement.
 
 from setuptools import find_packages, setup
 
