565d104afba2f12c8017faff9fca2658446f877e
Joseph Spisak <spisakjo@gmail.com>
2024-04-03 12:45:49 -0700

    Update test_tokenizer.py

--- llama/test_tokenizer.py
+++ llama/test_tokenizer.py
@@ -1,3 +1,6 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
+
 import os
 from unittest import TestCase
 from llama.tokenizer import ChatFormat, Tokenizer
