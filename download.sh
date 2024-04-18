8d5d2931be6ec573300bef100dfb1e2960e4fc46
xingjia01 <39668651+xingjia01@users.noreply.github.com>
2024-04-18 10:37:53 +0800

    update the weight folder name

--- download.sh
+++ download.sh
@@ -23,33 +23,37 @@ for m in ${MODEL_SIZE//,/ }
 do
     if [[ $m == "8B" ]] || [[ $m == "8b" ]]; then
         SHARD=0
+        MODEL_FOLDER_PATH="Meta-Llama-3-8B"
         MODEL_PATH="8b_pre_trained"
-    elif [[ $m == "8B-instruct" ]] || [[ $m == "8b-instruct" ]]; then
+    elif [[ $m == "8B-instruct" ]] || [[ $m == "8b-instruct" ]] || [[ $m == "8b-Instruct" ]] || [[ $m == "8B-Instruct" ]]; then
         SHARD=0
+        MODEL_FOLDER_PATH="Meta-Llama-3-8B-Instruct"
         MODEL_PATH="8b_instruction_tuned"
     elif [[ $m == "70B" ]] || [[ $m == "70b" ]]; then
         SHARD=7
+        MODEL_FOLDER_PATH="Meta-Llama-3-70B"
         MODEL_PATH="70b_pre_trained"
-    elif [[ $m == "70B-instruct" ]] || [[ $m == "70b-instruct" ]]; then
+    elif [[ $m == "70B-instruct" ]] || [[ $m == "70b-instruct" ]] || [[ $m == "70b-Instruct" ]] || [[ $m == "70B-Instruct" ]]; then
         SHARD=7
+        MODEL_FOLDER_PATH="Meta-Llama-3-70B-Instruct"
         MODEL_PATH="70b_instruction_tuned"
     fi
 
     echo "Downloading ${MODEL_PATH}"
-    mkdir -p ${TARGET_FOLDER}"/${MODEL_PATH}"
+    mkdir -p ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}"
 
     for s in $(seq -f "0%g" 0 ${SHARD})
     do
-        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/consolidated.${s}.pth"
+        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/consolidated.${s}.pth"
     done
 
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/params.json"
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/tokenizer.model"
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/checklist.chk"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/checklist.chk"
+    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/params.json"
+    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/tokenizer.model"
+    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/checklist.chk"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/checklist.chk"
     echo "Checking checksums"
     if [ "$CPU_ARCH" = "arm64" ]; then
-      (cd ${TARGET_FOLDER}"/${MODEL_PATH}" && md5 checklist.chk)
+      (cd ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}" && md5 checklist.chk)
     else
-      (cd ${TARGET_FOLDER}"/${MODEL_PATH}" && md5sum -c checklist.chk)
+      (cd ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}" && md5sum -c checklist.chk)
     fi
 done
