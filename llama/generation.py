006d52f2dd9dcf29db5fe2ffdc9c034b29e98992
JethroChow <tidesnn@gmail.com>
2024-07-03 10:07:19 +0800

    fix bugs

--- llama/generation.py
+++ llama/generation.py
@@ -167,7 +167,7 @@ class Llama:
         eos_reached = torch.tensor([False] * bsz, device="cuda")
         input_text_mask = tokens != pad_id
         if min_prompt_len == total_len:
-            logits, _ = self.model.forward(tokens, prev_pos)
+            logits = self.model.forward(tokens, prev_pos)
             token_logprobs = -F.cross_entropy(
                 input=logits.transpose(1, 2),
                 target=tokens,
@@ -178,7 +178,7 @@ class Llama:
         stop_tokens = torch.tensor(list(self.tokenizer.stop_tokens))
 
         for cur_pos in range(min_prompt_len, total_len):
-            logits, _ = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
+            logits  = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
             if temperature > 0:
                 probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
                 next_token = sample_top_p(probs, top_p)
