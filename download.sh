dd137efaa0c65c7e3a15d5918f3f55b6d282e0c3
Kiran Kumar Tirunandam Muralikrishna <123753360+tmkirankumar786@users.noreply.github.com>
2024-07-28 16:17:04 +0530

    License Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+# This software may be used and distributed according to the terms of the Llama 3 Community License Agreement.
 
 set -e
 
