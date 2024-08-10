19625e8bc144e073ca0b13b322daba9011320f8d
yuvraj042003 <yuvraj042003@gmail.com>
2024-08-10 20:59:31 +0530

    upgrade version llama3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(path: str):
 
 setup(
     name="llama3",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt"),
 )
